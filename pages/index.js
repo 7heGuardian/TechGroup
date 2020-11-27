2cf9d390c2ff052d60066e51ca7b53ee9d4312a5
ScriptKev <script.kev@gmail.com>
2020-11-27 18:59:59 -0500

    update names v2

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home() {
           <div style={{ textAlign: 'center', backgroundColor: '#fff', padding: '25px 20px', borderRadius: '10px', boxShadow: '1px 1px 15px #5656567d' }}>
             <h2 style={{ color: '#0070f3', fontWeight: '600' }}>Newman</h2>
             <h2 style={{ color: '#0070f3', fontWeight: '600' }}>Jose</h2>
+            <h2 style={{ color: '#0070f3', fontWeight: '600' }}>Jose 2</h2>
             <h2 style={{ color: '#0070f3', fontWeight: '600' }}>Josue</h2>
             <h2 style={{ color: '#0070f3', fontWeight: '600' }}>Kevin Blanco</h2>
           </div>
