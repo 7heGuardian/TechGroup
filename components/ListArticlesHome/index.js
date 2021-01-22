d7b92cf65172aeed3d269b6c30a5af7cd5e3a56d
7heGuardian <newmansanchez189@gmail.com>
2021-01-22 12:26:30 -0400

    Checking ListArticlesHome

--- components/ListArticlesHome/index.js
+++ components/ListArticlesHome/index.js
@@ -1,38 +1,23 @@
 import Link from 'next/link'
-import { ArticleStyled, ListArticlesLayoutStyled, ListArticlesStyled, ArticleThumbnail } from './styled'
+import { ListArticlesStyled,ArticleStyled } from './styled'
 
 export default function ListArticlesHome() {
-  return (
-    <ListArticlesLayoutStyled>
-      <span style={{ color: '#2b2d42', fontSize: '12px', fontWeight: '700' }}>MAS RECIENTE</span>
-
-      <ListArticlesStyled>
-        <li style={{ overflow: 'hidden' }}>
-          <ArticleStyled >
-            <ArticleThumbnail>
-              <Link href='/post/elaboracion-de-una-wiki'>
-                <a>
-                  <img style={{ borderRadius: '0px 16px' }} src='/1_DBff-fZ7TtXwf9r-Aem1_A.jpeg' alt='imagen' width='100%' height='144px' />
-                </a>
-              </Link>
-            </ArticleThumbnail>
-
-            <div style={{ width: '100%', height: 'min-content', margin: 'auto' }}>
-              <h2 style={{ fontFamily: 'Montserrat', fontSize: '24px', marginBottom: '24px'}}>
-                <Link href='/post/elaboracion-de-una-wiki'>
-                  <a>Elaboracion de una wiki</a>
-                </Link>
-              </h2>
-              <p style={{ fontWeight: '300', marginBottom: '24px' }}>
-                Una wiki es un sitio web cuyas páginas pueden ser editadas por múltiples voluntarios
-                a través del navegador web. Los usuarios pueden crear, modificar y borrar un mismo
-                texto que comparten.
-              </p>
-              <p>10 de noviembre del 2020</p>
-            </div>
-          </ArticleStyled>
-        </li>
-      </ListArticlesStyled>
-    </ListArticlesLayoutStyled>
+  return (              
+    <ListArticlesStyled>
+      <h3>MÁS RECIENTES</h3>
+      <Link href='/post/elaboracion-de-una-wiki'>
+        <ArticleStyled>
+          <img class='thumbnail' src='/1_DBff-fZ7TtXwf9r-Aem1_A_2.jpeg' width='320' alt='imagen'/>
+          <div class='info'>
+          <h2 class='tag'>WIKI</h2>
+          <h1 class='title'>Elaboracion de una wiki</h1>
+          <p class='description'>
+            Una wiki es un sitio web cuyas páginas pueden ser editadas por múltiples voluntarios a través del navegador web. Los usuarios pueden crear, modificar y borrar un mismo texto que comparten.
+          </p>
+          <p class='metadata'>10 de noviembre del 2020</p>
+          </div>
+        </ArticleStyled>
+      </Link>
+    </ListArticlesStyled>
   )
 }
--- components/ListArticlesHome/styled.js
+++ components/ListArticlesHome/styled.js
@@ -1,42 +1,77 @@
 import styled from '@emotion/styled'
 
-export const ListArticlesLayoutStyled = styled.section`
-  height: 100%;
+export const ListArticlesStyled = styled.section`
   width: 100%;
-  font-family: 'Roboto', sans-serif;
-  color: #000;
+
+  & h3 {
+    font-size: 12px;
+    margin-bottom: 24px;
+  }
 
   @media screen and (min-width: 320px) and (max-width: 900px) {
     grid-area: ListArticles;
   }
 `
 
-export const ListArticlesStyled = styled.ul`
-  width: 100%;
-  /* height: 100%; */
-  margin-top: 20px;
-  border-radius: 10px;
-`
-
 export const ArticleStyled = styled.article`
-  display: grid;
-  grid-template-columns: 250px 1fr;
-  grid-template-rows: 1fr;
   width: 100%;
-  column-gap: 20px;
+  cursor: pointer;
+
+  display: grid;
+  grid-template-columns: 2, 1fr;
+  grid-template-rows: auto;
+  grid-gap: 24px;
+
+  .thumbnail {
+    grid-column: 1;
+    grid-row: 1;
+
+    min-width: 240px;
+    max-width: 100%;
+    height: auto;
+    border-radius: 0px 16px;
+    object-fit: cover;
+  }
+
+  .info {
+    grid-column: 2;
+    grid-row: 1;
+
+    font-size: 16px;
+    line-height: 1.5;
+
+    .tag {
+      color: #FCA311;
+      font-size: 12px;
+    }
+
+    .title {
+      font-size: 24px;
+    }
+
+    .description {
+      font-weight: 300;
+      margin-bottom: 8px;
+    }
+
+    .metadata {
+      font-weight: regular;
+    }
+  }
+
 
   @media screen and (min-width: 320px) and (max-width: 550px) {
     grid-template-columns: 1fr;
-    grid-template-rows: 154px 1fr;
     justify-items: center;
-  }
-`
 
-export const ArticleThumbnail = styled.figure`
-  width: 240px;
-  height: 100%;
-  border-radius: 0px 16px;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+    .thumbnail {
+      grid-column: 1;
+      grid-row: 1;
+      width: 100%;
+    }
+    .info {
+      grid-column: 1;
+      grid-row: 2;
+    }
+  }
 `
